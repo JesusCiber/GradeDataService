4b5bb93f7a829959c068d573d78476abc5f5eb0d
JesusCiber <fjesuscl@gmail.com>
2025-05-19 18:14:18 +0200

    Init commit

---
+++ src/main/java/org/Ironhack/GradeDataServiceApplication.java
@@ -0,0 +1,9 @@
+package org.Ironhack;
+
+import org.springframework.boot.SpringApplication;
+
+public class GradeDataServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GradeDataServiceApplication.class, args);
+    }
+}

971b9ee7c9452c146204724de18dfa03a0b7a5d8
JesusCiber <fjesuscl@gmail.com>
2025-05-21 16:01:53 +0200

    Add JsonIgnore

--- src/main/java/com/example/demo/DTO/CourseGradeDTO.java
+++ src/main/java/com/example/demo/DTO/CourseGradeDTO.java
@@ -1,6 +1,7 @@
 package com.example.demo.DTO;
 
 import com.example.demo.Models.Grade;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- src/main/java/com/example/demo/Models/Grade.java
+++ src/main/java/com/example/demo/Models/Grade.java
@@ -1,5 +1,6 @@
 package com.example.demo.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,6 +18,7 @@ public class Grade {
 
     @ManyToOne
     @JoinColumn(name = "course_code")
+    @JsonIgnore
     private Course course;
 
     @Column(name = "student_id")
